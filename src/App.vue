8589fcff4ac2caabe81884188dbf356bf87a9d3e
Emilio Gallo <emilio.gallo17@gmail.com>
2024-05-08 14:27:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import { store } from "./store";
+
+import axios from "axios";
+
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+export default {
+
+  components: {
+
+    AppHeader,
+    AppMain,
+
+  },
+
+  data() {
+    return {
+
+      store,
+
+    };
+  },
+
+}
+</script>
+
+<template>
+
+  <AppHeader />
+
+  <AppMain />
+
+
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from "../store";
+
+
+export default {
+
+   components: {
+
+
+   },
+
+   data() {
+      return {
+
+         store,
+
+      };
+   },
+
+}
+</script>
+
+<template>
+
+   <i class="fa-solid fa-house"></i>
+
+   <h1>Header</h1>
+
+</template>
+
+<style scoped lang="scss"></style>
